5eca8f4a49d3d38ac8f4e3f73949fc48c1a31928
louiseClarkUSDS <louise.a.clark@omb.eop.gov>
2023-02-21 12:22:03 -0500

    adds cypress axe tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,38 @@
 const compareSnapshotCommand = require("cypress-visual-regression/dist/command");
 
 compareSnapshotCommand();
+
+const severityIndicators = {
+  minor:    '⚪️',
+  moderate: '🟡',
+  serious:  '🟠',
+  critical: '🔴',
+}
+
+function callback(violations) {
+  violations.forEach(violation => {
+    const nodes = Cypress.$(violation.nodes.map(node => node.target).join(','))
+
+    Cypress.log({
+      name: `${severityIndicators[violation.impact]} A11Y`,
+      consoleProps: () => violation,
+      $el: nodes,
+      message: `[${violation.help}](${violation.helpUrl})`
+    })
+
+    violation.nodes.forEach(({ target }) => {
+      Cypress.log({
+        name: '🔧',
+        consoleProps: () => violation,
+        $el: Cypress.$(target.join(',')),
+        message: target
+      })
+    })
+  });
+}
+
+Cypress.Commands.add("checkPageA11y", (path) => {
+  cy.visit(path);
+  cy.injectAxe();
+  cy.checkA11y(null, null, callback);
+})
