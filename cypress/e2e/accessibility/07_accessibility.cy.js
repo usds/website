2625eb9aa6549e14c0ea1f74153932b223063dcb
louiseClarkUSDS <louise.a.clark@omb.eop.gov>
2023-02-22 16:39:25 -0500

    adds accessibility full loop test

---
+++ cypress/e2e/accessibility/07_accessibility.cy.js
@@ -0,0 +1,12 @@
+const urls = require('../sitemap.json'); // see sitemap.sh
+
+describe('Entire Website accessibility', () => {
+  urls.forEach((url) => {
+    it(`${url.length === 0 ? '/' : url}`, () => {
+      // Wait for page to load ...
+      cy.intercept(url).as('getPage');
+      cy.visit(url);
+      cy.checkPageA11y(url);    
+      });
+    });
+  });
