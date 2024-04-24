88d7348fabc214d67e9205f2be43a2326083924f
TomNUSDS <74203452+TomNUSDS@users.noreply.github.com>
2024-04-24 11:40:44 -0700

    Fixes
    - Ignore www.usds.gov and usds.gov absolute links (impacts local dev and staging server)
    - Do not add icon to social links (but still target="_blank", etc)
    - Added .scss selectors to control positions better. Attempted to fix, but @drew-usds you should feel free to make changes to this branch to tweak.

--- assets/js/site/externallinks.js
+++ assets/js/site/externallinks.js
@@ -6,11 +6,24 @@ window.addEventListener('DOMContentLoaded', () => {
     const links = [...document.querySelectorAll(`a:not([href*='${origin}'])`)].filter(a => a.href.startsWith("https://"));
     for (const link of links) {
       try {
-        if (!(link instanceof HTMLElement)) {
+        // could be HTMLButtonElement or HTMLLinkElement or HTMLAnchorElement
+        if (!(link instanceof HTMLButtonElement || link instanceof HTMLAnchorElement || link instanceof HTMLLinkElement)) {
           continue;
         }
+        // for staging/dev instance, we don't want external links for fully qualified urls to usds.gov
+        // these are likely
+        if (link.href.startsWith("https://www.usds.gov") || link.href.startsWith("https://usds.gov")) {
+          continue;
+        }
+        // these are best practices for privacy/security
         link.setAttribute("target", "_blank");
         link.setAttribute("rel", "noopener noreferrer");
+
+        // do not add icon to these exceptions (social buttons)
+        if (link.classList.contains("site-c-social-buttons__link")) {
+          continue;
+        }
+
         link.classList.add("usa-link--external");
       } catch (err) {
         console.error(err);
